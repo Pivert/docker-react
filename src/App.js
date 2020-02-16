2671085a618abb5557ca116df9fda9fa44fd88d3
Francois Delpierre <job@pivert.org>
2020-02-16 23:03:09 +0000

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This has been changed, again ?
+          This has been changed, again, again?
         </p>
         <a
           className="App-link"
